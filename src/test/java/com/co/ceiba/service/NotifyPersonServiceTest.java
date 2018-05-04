8febb673b37198808937e48704aba2f76d8041fd
CEIBA\sebastian.vasquez <sebastian.vasquez@ceiba.com.co>
2018-05-04 17:19:37 -0500

    Commit de seguridad

--- src/test/java/com/co/ceiba/service/NotifyPersonServiceTest.java
+++ src/test/java/com/co/ceiba/service/NotifyPersonServiceTest.java
@@ -1,6 +1,7 @@
 package com.co.ceiba.service;
 
 import static com.co.ceiba.testdatabuilder.PersonTestDataBuilder.aPerson;
+import static org.mockito.Mockito.*;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -15,7 +16,8 @@ public class NotifyPersonServiceTest {
 	
 	@Before
 	public void setup() {
-		emailService = new EmailService();
+		//emailService = new EmailService();
+		emailService = mock(EmailService.class);
 		notifyPersonService = new NotifyPersonService(emailService);
 	}
 	
@@ -23,6 +25,8 @@ public class NotifyPersonServiceTest {
 	public void notifyTest() {
 		//Arrange
 		Person person = aPerson().build();
+		
+		when(emailService.sendMail(anyString())).thenReturn("Hola mundo");
 		//Act
 		String message = notifyPersonService.notify(person);
 		//Assert
